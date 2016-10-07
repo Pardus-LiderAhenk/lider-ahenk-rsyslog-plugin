e287bd090b9cbecbba9cc7cd986ac45752d8f9aa
Volkan Şahin <bm.volkansahin@gmail.com>
2016-10-07 11:01:50 +0300

    main module info added

--- ahenk-rsyslog/rsyslog/main.py
+++ ahenk-rsyslog/rsyslog/main.py
@@ -3,4 +3,14 @@
 
 
 def info():
-    return None
+    inf = dict()
+    inf['name'] = 'rsyslog'
+    inf['version'] = '1.0.0'
+    inf['support'] = 'debian'
+    inf['description'] = ''
+    inf['task'] = False
+    inf['user_oriented'] = False
+    inf['machine_oriented'] = True
+    inf['developer'] = ''
+
+    return inf
