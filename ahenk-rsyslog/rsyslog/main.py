5157bf8fed1029b1fcafeeec354115a937bdff6f
cemrealpsoy <cemrealpsoy@gmail.com>
2016-10-10 17:40:55 +0300

    Plugin desc and developer info are added to main.py

--- ahenk-rsyslog/rsyslog/main.py
+++ ahenk-rsyslog/rsyslog/main.py
@@ -7,10 +7,10 @@ def info():
     inf['name'] = 'rsyslog'
     inf['version'] = '1.0.0'
     inf['support'] = 'debian'
-    inf['description'] = ''
+    inf['description'] = 'System Log file configuration and rotation info are defined with this plugin.'
     inf['task'] = False
     inf['user_oriented'] = False
     inf['machine_oriented'] = True
-    inf['developer'] = ''
+    inf['developer'] = 'cemre.alpsoy@agem.com.tr'
 
     return inf
